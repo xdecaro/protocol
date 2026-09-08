38fee95ea7d548df3c708ade189a5f951cfbed86
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 18:15:59 +0200

    Scope Protocol register form to Core UI tokens

--- component/admin/tmpl/register/edit.php
+++ component/admin/tmpl/register/edit.php
@@ -7,7 +7,7 @@ use Joomla\CMS\Router\Route;
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
 ?>
-<form action="<?php echo Route::_('index.php?option=com_decaroprotocol&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="register-form" class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_decaroprotocol&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="register-form" class="form-validate xdecaro-scope decaroprotocol-admin">
     <div class="card"><div class="card-body"><?php echo $this->form->renderFieldset('details'); ?></div></div>
     <input type="hidden" name="task" value="">
     <?php echo HTMLHelper::_('form.token'); ?>
