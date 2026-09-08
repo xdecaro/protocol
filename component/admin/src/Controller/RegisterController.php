869f8f964abdcc55b65990bbee26c3dec0a00bc1
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 17:57:10 +0200

    Keep register form controller focused on add and edit

--- component/admin/src/Controller/RegisterController.php
+++ component/admin/src/Controller/RegisterController.php
@@ -3,7 +3,6 @@ namespace Xdecaro\Component\Decaroprotocol\Administrator\Controller;
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\FormController;
 
 class RegisterController extends FormController
@@ -18,14 +17,5 @@ class RegisterController extends FormController
     protected function allowEdit($data = array(), $key = 'id')
     {
         return $this->app->getIdentity()->authorise('protocol.manage_registers', 'com_decaroprotocol') && parent::allowEdit($data, $key);
-    }
-
-    public function delete()
-    {
-        if (!$this->app->getIdentity()->authorise('protocol.manage_registers', 'com_decaroprotocol')) {
-            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
-        }
-
-        return parent::delete();
     }
 }
