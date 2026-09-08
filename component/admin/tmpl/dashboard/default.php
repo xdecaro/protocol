d5d10a73babbb8405dbdc565e1978bf90fc7091b
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 18:15:18 +0200

    Scope Protocol dashboard to Core UI tokens

--- component/admin/tmpl/dashboard/default.php
+++ component/admin/tmpl/dashboard/default.php
@@ -13,14 +13,16 @@ $cards = array(
     'internal' => 'COM_DECAROPROTOCOL_DIRECTION_INTERNAL',
 );
 ?>
-<div class="decaroprotocol-dashboard">
-    <?php foreach ($cards as $key => $label) : ?>
-        <div class="decaroprotocol-card">
-            <div class="decaroprotocol-card__value"><?php echo (int) ($this->stats[$key] ?? 0); ?></div>
-            <div class="decaroprotocol-card__label"><?php echo Text::_($label); ?></div>
-        </div>
-    <?php endforeach; ?>
-</div>
-<div class="mt-4">
-    <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_decaroprotocol&view=records'); ?>"><?php echo Text::_('COM_DECAROPROTOCOL_OPEN_RECORDS'); ?></a>
+<div class="xdecaro-scope decaroprotocol-admin">
+    <div class="decaroprotocol-dashboard">
+        <?php foreach ($cards as $key => $label) : ?>
+            <div class="decaroprotocol-card">
+                <div class="decaroprotocol-card__value"><?php echo (int) ($this->stats[$key] ?? 0); ?></div>
+                <div class="decaroprotocol-card__label"><?php echo Text::_($label); ?></div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <div class="mt-4">
+        <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_decaroprotocol&view=records'); ?>"><?php echo Text::_('COM_DECAROPROTOCOL_OPEN_RECORDS'); ?></a>
+    </div>
 </div>
