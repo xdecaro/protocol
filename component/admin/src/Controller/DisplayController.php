3621d382a2e0a0900dee8ece8ad7b1c251718cba
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 17:58:55 +0200

    Enforce backend access ACL in display controller

--- component/admin/src/Controller/DisplayController.php
+++ component/admin/src/Controller/DisplayController.php
@@ -3,9 +3,19 @@ namespace Xdecaro\Component\Decaroprotocol\Administrator\Controller;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 
 class DisplayController extends BaseController
 {
     protected $default_view = 'dashboard';
+
+    public function display($cachable = false, $urlparams = array())
+    {
+        if (!$this->app->getIdentity()->authorise('core.manage', 'com_decaroprotocol')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        return parent::display($cachable, $urlparams);
+    }
 }
