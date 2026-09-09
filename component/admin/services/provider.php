2c9fe87d3b3dff500da97108e87f40cd4e88cce5
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-09 14:32:01 +0200

    Wire Protocol integration services into component

--- component/admin/services/provider.php
+++ component/admin/services/provider.php
@@ -8,14 +8,16 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
-use Joomla\CMS\Extension\MVCComponent;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Xdecaro\Component\Decaroprotocol\Administrator\Extension\ProtocolComponent;
+use Xdecaro\Component\Decaroprotocol\Administrator\Service\AnalyticsSourceService;
 use Xdecaro\Component\Decaroprotocol\Administrator\Service\CoreIntegrationService;
+use Xdecaro\Component\Decaroprotocol\Administrator\Service\CrossProductIntegrationService;
 use Xdecaro\Component\Decaroprotocol\Administrator\Service\DocumentsIntegrationService;
 use Xdecaro\Component\Decaroprotocol\Administrator\Service\ProtocolService;
 
@@ -27,16 +29,9 @@ return new class () implements ServiceProviderInterface {
 
         $container->share(
             ProtocolService::class,
-            function (Container $container) {
-                return new ProtocolService($container->get(DatabaseInterface::class));
-            }
+            static fn (Container $container): ProtocolService => new ProtocolService($container->get(DatabaseInterface::class))
         );
-
-        $container->share(
-            CoreIntegrationService::class,
-            static fn (): CoreIntegrationService => new CoreIntegrationService()
-        );
-
+        $container->share(CoreIntegrationService::class, static fn (): CoreIntegrationService => new CoreIntegrationService());
         $container->share(
             DocumentsIntegrationService::class,
             static fn (Container $container): DocumentsIntegrationService => new DocumentsIntegrationService(
@@ -44,12 +39,27 @@ return new class () implements ServiceProviderInterface {
                 $container->get(DatabaseInterface::class)
             )
         );
+        $container->share(
+            AnalyticsSourceService::class,
+            static fn (Container $container): AnalyticsSourceService => new AnalyticsSourceService(
+                $container->get(DatabaseInterface::class)
+            )
+        );
+        $container->share(
+            CrossProductIntegrationService::class,
+            static fn (): CrossProductIntegrationService => new CrossProductIntegrationService()
+        );
 
         $container->set(
             ComponentInterface::class,
-            function (Container $container) {
-                $component = new MVCComponent($container->get(ComponentDispatcherFactoryInterface::class));
+            static function (Container $container): ComponentInterface {
+                $component = new ProtocolComponent($container->get(ComponentDispatcherFactoryInterface::class));
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                $component->setProtocolService($container->get(ProtocolService::class));
+                $component->setCoreIntegrationService($container->get(CoreIntegrationService::class));
+                $component->setDocumentsIntegrationService($container->get(DocumentsIntegrationService::class));
+                $component->setAnalyticsSourceService($container->get(AnalyticsSourceService::class));
+                $component->setCrossProductIntegrationService($container->get(CrossProductIntegrationService::class));
 
                 return $component;
             }
